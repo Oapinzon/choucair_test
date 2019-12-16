4704dd1db2cb8c4c901ae623d5309336c0573716
Oapinzon <oapinzon@hotmail.com>
2019-12-15 22:03:39 -0500

    Add files via upload

---
+++ prueba2.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+driver = webdriver.Chrome(executable_path=r"C:\driver_chrome\chromedriver.exe")
+driver.get("https://www.choucairtesting.com/empleos-2/")
+
+driver.execute_script("window.scrollTo(0,500)")
+time.sleep(3)
+driver.close()
